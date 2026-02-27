ba175c4f19bb28ab3e0f8bc9dcfb1f9850351447
schplitt <schplitt.contact@gmail.com>
2026-02-27 12:52:43 +0100

    feat: update nitro configuration for Cloudflare deployment and add wrangler dependency

--- website/nitro.config.ts
+++ website/nitro.config.ts
@@ -2,4 +2,14 @@ import { defineConfig } from 'nitro'
 
 export default defineConfig({
   serverDir: './server',
+  compatibilityDate: '2026-01-01',
+
+  cloudflare: {
+    deployConfig: true,
+    nodeCompat: true,
+    wrangler: {
+      name: 'clank8y-website',
+
+    },
+  },
 })
