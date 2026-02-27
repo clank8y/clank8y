0a987a96d15f1ee59d01aa252c6083074667580f
schplitt <schplitt.contact@gmail.com>
2026-02-27 16:09:52 +0100

    chore: more linting ignores

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import schplitt from '@schplitt/eslint-config'
 
-export default schplitt().overrideRules({
+export default schplitt({
+  ignores: ['dist', 'build', 'coverage', 'node_modules', '.output', '.wrangler'],
+}).overrideRules({
   'no-console': 'off',
 })
