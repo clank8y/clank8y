5cba4112c8d50df58f9fbf56b7f4e505fe135eb3
schplitt <schplitt.contact@gmail.com>
2026-03-17 14:51:23 +0100

    ignore .clank8y

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import schplitt from '@schplitt/eslint-config'
 
 export default schplitt({
-  ignores: ['dist', 'build', 'coverage', 'node_modules', '.output', '.wrangler'],
+  ignores: ['dist', 'build', 'coverage', 'node_modules', '.output', '.wrangler', '.clank8y'],
 }).overrideRules({
   'no-console': 'off',
 })
